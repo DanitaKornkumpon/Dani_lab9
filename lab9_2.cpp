0e13635bf25418693135f6f2129c77f3b67c48aa
Wimwipa780 <wimwipha4746@gmail.com>
2024-12-17 13:38:05 +0700

    Lab_9.2

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,7 +1,19 @@
 #include<iostream>
 using namespace std;
+void printO(int N, int M) {
+    
+    if (N <= 0 || M <= 0) {
+        cout << "Invalid input" << endl;
+        return;
+    }
 
-//Write the function printO() here
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            cout << "O";
+        }
+        cout << endl;  
+    }
+}
 
 int main(){
 	
